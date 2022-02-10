0d1b19332049f8b7ca943a9fc30426950845703f
sanjay <sanjay.ma@bixbytessolutions.com>
2022-02-10 19:03:48 +0530

    initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+
+$('.project_img_carousel').owlCarousel({
+    loop: true,
+    margin:10,
+    nav: false,
+    items: 1.5,
+    center: true,
+    dots: false
+});
+
+
+$('.project_technology_carousel').owlCarousel({
+    loop: true,
+    margin:10,
+    nav: false,
+    center: true,
+    dots: false
+});
+
